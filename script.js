7d7c80ae3f0f6d9c665abeb4064d514ed469f019
Anshuman Varma <anshumanverma9795@gmail.com>
2026-02-06 06:57:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -198,7 +198,7 @@ document.addEventListener('DOMContentLoaded', function () {
             github: 'https://github.com/Anshuman892494/ExamPoint',
             // demo: 'https://exampoint-frontend.onrender.com/',
             demo: 'https://acciexam.in',
-            image: 'https://github.com/Anshuman892494/ExamPoint/blob/main/Screenshots/student_login.png?raw=true'
+            image: 'https://github.com/Anshuman892494/Portfolio_2/blob/main/img/Screenshot%202026-02-06%20065612.png?raw=true'
         },
         {
             id: 9,
